6c076070fd133c0d2693787824de3d35da35355b
imjayapple <imjayapple@gmail.com>
2024-01-01 20:47:45 -0800

    Manual test cases without Jest

--- basic-unit-test.js
+++ basic-unit-test.js
@@ -38,4 +38,14 @@ test('findMax function handles equal numbers correctly', () => {
   expect(findMax(10, 10)).toBe(10);
 });
 
-// npx jest  
+// npx jest
+
+const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a+b) === 100;
+console.log(isEqualTo100(100,0));
+console.log(isEqualTo100(0,100));
+
+console.log(isEqualTo100(10,0));
+console.log(isEqualTo100(0,10));
+
+console.log(isEqualTo100(20,80));
+console.log(isEqualTo100(50,50));
